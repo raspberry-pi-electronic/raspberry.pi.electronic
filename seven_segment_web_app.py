345cec02a9854417a1c09b6c05c918a2a19e26bb
Cheng Yan <chng.yan.2909@gmail.com>
2024-06-29 15:32:35 -0500

    initial web app test

--- seven_segment_web_app.py
+++ seven_segment_web_app.py
@@ -1,2 +1,13 @@
 from flask import Flask, request
-from two-digit-num import *
+from two_digit_num import *
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+
